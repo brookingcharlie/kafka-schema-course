9842217cd409444b350939ac255ad2929eea9f48
Charlie Brooking <cbrookin@thoughtworks.com>
2020-08-01 23:15:37 +1000

    add file IO to specific record example

--- code/avro-examples/src/main/java/org/example/specific/SpecificRecordExamples.java
+++ code/avro-examples/src/main/java/org/example/specific/SpecificRecordExamples.java
@@ -1,6 +1,15 @@
 package org.example.specific;
 
 import com.example.Customer;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SpecificRecordExamples {
     public static void main(String[] args) {
@@ -13,5 +22,28 @@ public class SpecificRecordExamples {
         customerBuilder.setAutomatedEmail(false);
         Customer customer = customerBuilder.build();
         System.out.println(customer);
+
+        File file = new File("customer-specific.avro");
+
+        DatumWriter<Customer> datumWriter = new SpecificDatumWriter<>(Customer.class);
+        try (DataFileWriter<Customer> fileWriter = new DataFileWriter<>(datumWriter)) {
+            fileWriter.create(customer.getSchema(), file);
+            fileWriter.append(customer);
+            System.out.println("successfully wrote customer-specific.avro");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final DatumReader<Customer> datumReader = new SpecificDatumReader<>(Customer.class);
+        try (DataFileReader<Customer> fileReader = new DataFileReader<>(file, datumReader)) {
+            System.out.println("Reading our specific record");
+            while (fileReader.hasNext()) {
+                Customer readCustomer = fileReader.next();
+                System.out.println(readCustomer.toString());
+                System.out.println("First name: " + readCustomer.getFirstName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
