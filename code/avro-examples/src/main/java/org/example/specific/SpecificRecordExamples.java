30fec527d48e098111a37559c06d55117b7c7ea7
Charlie Brooking <cbrookin@thoughtworks.com>
2020-08-01 23:17:36 +1000

    extract methods for specific records example

--- code/avro-examples/src/main/java/org/example/specific/SpecificRecordExamples.java
+++ code/avro-examples/src/main/java/org/example/specific/SpecificRecordExamples.java
@@ -13,27 +13,13 @@ import java.io.IOException;
 
 public class SpecificRecordExamples {
     public static void main(String[] args) {
-        Customer.Builder customerBuilder = Customer.newBuilder();
-        customerBuilder.setAge(25);
-        customerBuilder.setFirstName("John");
-        customerBuilder.setLastName("Doe");
-        customerBuilder.setHeight(175.5f);
-        customerBuilder.setWeight(80.5f);
-        customerBuilder.setAutomatedEmail(false);
-        Customer customer = customerBuilder.build();
-        System.out.println(customer);
-
+        Customer customer = createCustomer();
         File file = new File("customer-specific.avro");
+        writeToFile(customer, file);
+        readFromFile(file);
+    }
 
-        DatumWriter<Customer> datumWriter = new SpecificDatumWriter<>(Customer.class);
-        try (DataFileWriter<Customer> fileWriter = new DataFileWriter<>(datumWriter)) {
-            fileWriter.create(customer.getSchema(), file);
-            fileWriter.append(customer);
-            System.out.println("successfully wrote customer-specific.avro");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    private static void readFromFile(File file) {
         final DatumReader<Customer> datumReader = new SpecificDatumReader<>(Customer.class);
         try (DataFileReader<Customer> fileReader = new DataFileReader<>(file, datumReader)) {
             System.out.println("Reading our specific record");
@@ -45,5 +31,29 @@ public class SpecificRecordExamples {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void writeToFile(Customer customer, File file) {
+        DatumWriter<Customer> datumWriter = new SpecificDatumWriter<>(Customer.class);
+        try (DataFileWriter<Customer> fileWriter = new DataFileWriter<>(datumWriter)) {
+            fileWriter.create(customer.getSchema(), file);
+            fileWriter.append(customer);
+            System.out.println("successfully wrote customer-specific.avro");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Customer createCustomer() {
+        Customer.Builder customerBuilder = Customer.newBuilder();
+        customerBuilder.setAge(25);
+        customerBuilder.setFirstName("John");
+        customerBuilder.setLastName("Doe");
+        customerBuilder.setHeight(175.5f);
+        customerBuilder.setWeight(80.5f);
+        customerBuilder.setAutomatedEmail(false);
+        Customer customer = customerBuilder.build();
+        System.out.println(customer);
+        return customer;
     }
 }
